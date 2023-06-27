7930d7eb2f474cc8a08011d26d2c4fadd4ce2293
bonbongiraffe <francescowai@yahoo.com>
2023-06-27 15:07:17 -0400

    login tweaks and activity stuff continued

--- src/components/App.js
+++ src/components/App.js
@@ -6,39 +6,60 @@ import { useState, useEffect } from "react";
 import Nav from "./Nav";
 import Activities from "./Activities";
 import Login from "./Login";
+import Cart from "./Cart";
 
 const url =  "http://localhost:3000"
 
 function App() {
   const [ activities, setActivities ] = useState({})
-  const [ currentUser, setCurrentUser ] = useState({})
+  const [ user, setUser ] = useState({})
 
+  //fetch activities
   useEffect(()=>{
     fetch(`${url}/activities`)
       .then(r => r.json())
       .then(activitiesObj => setActivities(activitiesObj))
   },[])
 
+  //fetch current user
   useEffect(()=>{
     fetch(`${url}/users`)
       .then(r => r.json())
-      .then(users => setCurrentUser(
-        users.find(user => user.submitted === false) ? users.find(user => user.submitted === false) : {}))
+      .then(users => {
+        setUser((user => user.submitted === false) ? users.find(user => user.submitted === false) : {})
+      })
   },[])
+
+  const addActivity = (newActivity) => {
+    fetch(`${url}/users/${user.id}`,{
+      method: "PATCH",
+      headers: { "Content-Type" : "application/json" },
+      body: JSON.stringify({ 
+        activities: [...user.activities, newActivity], 
+        wallet: user.wallet-newActivity.cost,
+        location: newActivity.destination
+      })
+    })
+      .then( r => r.json())
+      .then( updatedUser => setUser(updatedUser))
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-      {currentUser.name ? (
+      {user.name ? (
       <>
-        <Nav user={currentUser}/>
+        <Nav user={user}/>
         <Switch>
           <Route path="/activities">
-            <Activities activities={activities}/>
+            <Activities location={user.location} activities={activities} addActivity={addActivity}/>
+          </Route>
+          <Route path="/current">
+
           </Route>
         </Switch>
       </>      
-      ) : <Login setCurrentUser={setCurrentUser}/> } 
+      ) : <Login setUser={setUser}/> } 
       </header>
     </div>
   );
